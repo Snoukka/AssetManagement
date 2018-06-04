98e709acbf54d123fd9294b0da295865f8456969
Jyri Mattila <jyri.mattila@stud.pointcollege.fi>
2018-06-04 14:16:55 +0300

    Lisätty Entity-malli projektiin.

---
+++ AssetManagementWeb/SQLGreateTables.sql
@@ -0,0 +1,24 @@
+﻿CREATE TABLE [dbo].[AssetLocation]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY (1,1),
+	[Code] NVARCHAR (100),
+	[Name] NVARCHAR (200),
+	[Address] NVARCHAR (500),
+
+)
+CREATE TABLE [dbo].[Assets]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY (1,1),
+	[Code] NVARCHAR (100),
+	[Type] NVARCHAR (200),
+	[Model] NVARCHAR (500),
+
+)
+CREATE TABLE [dbo].[AssetLocations]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY (1,1),
+	[LocationId] INT,
+	[AssetId] INT,
+	[LastSeen] DATETIME
+
+)
